e0e40f1e32f49b6f6bb2137e354612dac914dfa2
tchipi88 <ngansop.arthur@gmail.com>
2018-04-05 00:21:25 +0200

    v1.1

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,21 +13,25 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var company = $("input#company").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+
+          
+            
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
-                    company: company,
                     email: email,
-                    message: message
+                    message: message,
+                    //THIS WILL TELL THE FORM IF THE USER IS CAPTCHA VERIFIED.
+                   captcha: grecaptcha.getResponse()
+
                 },
                 cache: false,
                 success: function() {
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['company']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,15 +11,40 @@ if(empty($_POST['name'])  		||
 	
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$company = strip_tags(htmlspecialchars($_POST['company']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
+
+//for secret key for test
+//$secret="6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+$secret="6Ld8NFAUAAAAAIQ11VUSZ1d9cLX1wR3fSwLzI2sz";
+
+$response=$_POST["captcha"];
+
+$verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
+
+$captcha_success=json_decode($verify);
+
+if ($captcha_success->success==false) {
+
+  //This user was not verified by recaptcha.
+  alert('Your are a robot');
+
+}
+
+else if ($captcha_success->success==true) {
+
+  //This user is verified by recaptcha
 // Create the email and send the message
 $to = 'ganeo.app@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nCompany: $company\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
+ 
+
+}
+
+	
+			
 ?>
